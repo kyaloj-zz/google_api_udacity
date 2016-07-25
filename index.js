f50ed67d62a91e35ce24239231f5439a122efb35
Justus <mj.kyalo@gmail.com>
2016-07-25 17:01:57 +0300

    implementing google streat view on web

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var map;
-// Create a new blank array for all the listing markers.
 var markers = [];
 function initMap() {
-  // Create a styles array to use with the map.
   var styles = [
     {
       featureType: 'water',
@@ -26,7 +24,7 @@ function initMap() {
       featureType: 'road.highway',
       elementType: 'geometry.stroke',
       stylers: [
-        { color: '#000000' },
+        { color: '#efe9e4' },
         { lightness: -40 }
       ]
     },{
@@ -67,44 +65,24 @@ function initMap() {
         { color: '#efe9e4' },
         { lightness: -25 }
       ]
-    },{
-      featureType: 'landscape',
-      elementType: 'geometry',
-      stylers: [
-        { hue: '#ffff00' },
-        { gamma: 1.4 },
-        { saturation: 82 },
-        { lightness: 96 }
-      ]
     }
   ];
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -1.2681343, lng: 36.8059169},
+    center: {lat: 44.928628, lng: -93.286051},
     zoom: 13,
     styles: styles,
     mapTypeControl: false
   });
-  // These are the real estate listings that will be shown to the user.
-  // Normally we'd have these in a database instead.
   var locations = [
-    {title: 'Focus mobile', location: {lat: -1.2681343, lng: 36.8059169}},
-    {title: 'Focus mobile', location: {lat: -1.268208, lng: 36.808057}},
-    {title: 'Sopa lodges', location: {lat: -1.263542, lng: 36.808057}}
+    {title: 'Park', location: {lat: 44.928827, lng: -93.286306}},
   ];
   var largeInfowindow = new google.maps.InfoWindow();
-  // Style the markers a bit. This will be our listing marker icon.
   var defaultIcon = makeMarkerIcon('0091ff');
-  // Create a "highlighted location" marker color for when the user
-  // mouses over the marker.
   var highlightedIcon = makeMarkerIcon('FFFF24');
-  var largeInfowindow = new google.maps.InfoWindow();
-  // The following group uses the location array to create an array of markers on initialize.
   for (var i = 0; i < locations.length; i++) {
-    // Get the position from the location array.
     var position = locations[i].location;
     var title = locations[i].title;
-    // Create a marker per location, and put into markers array.
     var marker = new google.maps.Marker({
       position: position,
       title: title,
@@ -112,14 +90,10 @@ function initMap() {
       icon: defaultIcon,
       id: i
     });
-    // Push the marker to our array of markers.
     markers.push(marker);
-    // Create an onclick event to open the large infowindow at each marker.
     marker.addListener('click', function() {
       populateInfoWindow(this, largeInfowindow);
     });
-    // Two event listeners - one for mouseover, one for mouseout,
-    // to change the colors back and forth.
     marker.addListener('mouseover', function() {
       this.setIcon(highlightedIcon);
     });
@@ -130,37 +104,52 @@ function initMap() {
   document.getElementById('show-listings').addEventListener('click', showListings);
   document.getElementById('hide-listings').addEventListener('click', hideListings);
 }
-
 function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
+    infowindow.setContent('');
     infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>' + '<div>' 
-    	+ 'Lat: ' + marker.getPosition().lat() + ', Long: ' +  marker.getPosition().lng() + '</div>');
-    infowindow.open(map, marker);
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick',function(){
-      infowindow.setMarker(null);
+    infowindow.addListener('closeclick', function() {
+      infowindow.marker = null;
     });
+    var streetViewService = new google.maps.StreetViewService();
+    var radius = 50;
+    function getStreetView(data, status) {
+      if (status == google.maps.StreetViewStatus.OK) {
+        var nearStreetViewLocation = data.location.latLng;
+        var heading = google.maps.geometry.spherical.computeHeading(
+          nearStreetViewLocation, marker.position);
+          infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+          var panoramaOptions = {
+            position: nearStreetViewLocation,
+            pov: {
+              heading: heading,
+              pitch: 30
+            }
+          };
+        var panorama = new google.maps.StreetViewPanorama(
+          document.getElementById('pano'), panoramaOptions);
+      } else {
+        infowindow.setContent('<div>' + marker.title + '</div>' +
+          '<div>No Street View Found</div>');
+      }
+    }
+    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+    infowindow.open(map, marker);
   }
-}   
-// This function will loop through the markers array and display them all.
+}
 function showListings() {
   var bounds = new google.maps.LatLngBounds();
-  // Extend the boundaries of the map for each marker and display the marker
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(map);
     bounds.extend(markers[i].position);
   }
   map.fitBounds(bounds);
 }
-// This function will loop through the listings and hide them all.
 function hideListings() {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
 }
-
 function makeMarkerIcon(markerColor) {
   var markerImage = new google.maps.MarkerImage(
     'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
